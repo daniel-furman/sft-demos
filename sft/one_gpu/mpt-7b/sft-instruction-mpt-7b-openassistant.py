0a3da78e8be4cf6485e3ce25ba9e9c1c3b0cdff7
daniel-furman <dryanfurman@gmail.com>
2023-07-02 15:34:23 -0400

    train scripts re-written as fcns

--- sft/one_gpu/mpt-7b/sft-instruction-mpt-7b-openassistant.py
+++ sft/one_gpu/mpt-7b/sft-instruction-mpt-7b-openassistant.py
@@ -45,181 +45,218 @@ import torch
 import transformers
 from datasets import load_dataset
 from trl import SFTTrainer
-
-# print GPU available memory
-
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max_memory: ", max_memory)
-
-"""
-## Dataset
-
-For our experiment, we will use the `timdettmers/openassistant-guanaco` dataset to train general purpose instruct model.
-
-The dataset can be found [here](https://huggingface.co/datasets/timdettmers/openassistant-guanaco)
-"""
-
-dataset_name = "timdettmers/openassistant-guanaco"
-print(f"\nLoading {dataset_name} dataset...")
-dataset_openassistant = load_dataset(dataset_name)
-prompts = []
-responses = []
-for i in range(len(dataset_openassistant["train"])):
-    conversation = dataset_openassistant["train"][i]["text"]
-    # grab first human / assistant interaction
-    prompt = conversation.split("### Human: ")[1].split("### Assistant: ")[0]
-    prompts.append(prompt)
-    response = conversation.split("### Assistant: ")[1].split("### Human: ")[0]
-    responses.append(response)
-
-dataset_openassistant["train"] = dataset_openassistant["train"].add_column(
-    "prompt", prompts
-)
-dataset_openassistant["train"] = dataset_openassistant["train"].add_column(
-    "response", responses
-)
-
-# remove old text cols
-dataset_openassistant["train"] = dataset_openassistant["train"].remove_columns(
-    [
-        col
-        for col in dataset_openassistant["train"].column_names
-        if col not in ["prompt", "response"]
-    ]
-)
-
-print("Print an example in the train dataset:")
-print(dataset_openassistant["train"])
-print(dataset_openassistant["train"][0])
-
-print("Final train dataset:")
-train_dataset = dataset_openassistant["train"].shuffle(seed=42)
-print(train_dataset)
-print(train_dataset[0])
-print(train_dataset[-1])
-
-# let's now write a function to format the dataset for instruction fine-tuning
-# we will use the mpt-instruct model docs format
-# see https://huggingface.co/docs/trl/main/en/sft_trainer#format-your-input-prompts for docs
+import argparse
 
 
-def formatting_prompts_func(dataset):
-    instructions = []
-    for i in range(len(dataset["prompt"])):
-        text = f"{dataset['prompt'][i]}\n{dataset['response'][i]}"
-        instructions.append(text)
-    return instructions
+def training_function(args):
+    lr = args.lr
+    num_epochs = args.num_epochs
+    seed = args.seed
+
+    transformers.set_seed(seed)
+
+    # print GPU available memory
+
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    """
+    ## Dataset
+    
+    For our experiment, we will use the `timdettmers/openassistant-guanaco` dataset to train general purpose instruct model.
+    
+    The dataset can be found [here](https://huggingface.co/datasets/timdettmers/openassistant-guanaco)
+    """
+
+    dataset_name = "timdettmers/openassistant-guanaco"
+    print(f"\nLoading {dataset_name} dataset...")
+    dataset_openassistant = load_dataset(dataset_name)
+    prompts = []
+    responses = []
+    for i in range(len(dataset_openassistant["train"])):
+        conversation = dataset_openassistant["train"][i]["text"]
+        # grab first human / assistant interaction
+        prompt = conversation.split("### Human: ")[1].split("### Assistant: ")[0]
+        prompts.append(prompt)
+        response = conversation.split("### Assistant: ")[1].split("### Human: ")[0]
+        responses.append(response)
+
+    dataset_openassistant["train"] = dataset_openassistant["train"].add_column(
+        "prompt", prompts
+    )
+    dataset_openassistant["train"] = dataset_openassistant["train"].add_column(
+        "response", responses
+    )
+
+    # remove old text cols
+    dataset_openassistant["train"] = dataset_openassistant["train"].remove_columns(
+        [
+            col
+            for col in dataset_openassistant["train"].column_names
+            if col not in ["prompt", "response"]
+        ]
+    )
+
+    print("Print an example in the train dataset:")
+    print(dataset_openassistant["train"])
+    print(dataset_openassistant["train"][0])
+
+    print("Final train dataset:")
+    train_dataset = dataset_openassistant["train"].shuffle(seed=42)
+    print(train_dataset)
+    print(train_dataset[0])
+    print(train_dataset[-1])
+
+    # let's now write a function to format the dataset for instruction fine-tuning
+    # we will use the mpt-instruct model docs format
+    # see https://huggingface.co/docs/trl/main/en/sft_trainer#format-your-input-prompts for docs
+
+    def formatting_prompts_func(dataset):
+        instructions = []
+        for i in range(len(dataset["prompt"])):
+            text = f"{dataset['prompt'][i]}\n{dataset['response'][i]}"
+            instructions.append(text)
+        return instructions
+
+    """
+    ## Loading the model
+    
+    In this section we will load the [MPT-7B model](https://huggingface.co/mosaicml/mpt-7b).
+    """
+
+    # load assets
+
+    model_id = "mosaicml/mpt-7b"
+
+    # mpt tokenizer load
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+    # set mpt tokenizer padding token to eos token
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    print(f"{model_id} tokenizer model_max_length: ", tokenizer.model_max_length)
+
+    # mpt llm load
+    config = transformers.AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+
+    # custom options
+    config.attn_config["attn_impl"] = "triton"  # Optional triton attention
+    config.init_device = "meta"  # For fast initialization
+    config.max_seq_len = tokenizer.model_max_length
+    config.torch_dtype = "bfloat16"  # Set bfloat16 data type for sft
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        config=config,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    """
+    
+    ## Loading the trainer
+    
+    Here we will use the [`SFTTrainer` from TRL library](https://huggingface.co/docs/trl/main/en/sft_trainer) that gives a wrapper around transformers `Trainer` to easily fine-tune models on instruction based datasets. Let's first load the training arguments below.
+    from transformers import TrainingArguments
+    # see https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
+    """
+
+    output_dir = "./results"
+    num_train_epochs = num_epochs
+    auto_find_batch_size = True
+    gradient_accumulation_steps = 1
+    optim = "adamw_torch"
+    save_strategy = "epoch"
+    learning_rate = lr
+    lr_scheduler_type = "linear"
+    warmup_ratio = 0.03
+    logging_strategy = "steps"
+    logging_steps = 50
+
+    training_arguments = transformers.TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=num_train_epochs,
+        auto_find_batch_size=auto_find_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim=optim,
+        save_strategy=save_strategy,
+        learning_rate=learning_rate,
+        lr_scheduler_type=lr_scheduler_type,
+        warmup_ratio=warmup_ratio,
+        logging_strategy=logging_strategy,
+        logging_steps=logging_steps,
+    )
+
+    """
+    Then finally pass everything to the trainer
+    """
+
+    max_seq_length = tokenizer.model_max_length
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset,
+        formatting_func=formatting_prompts_func,
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+    )
+
+    """
+    ## Train the model
+    
+    Now let's train the model! Simply call `trainer.train()`
+    """
+
+    trainer.train()
+
+    # finished: print GPU available memory and total time
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    end = time.time()
+    print("Total time (sec): ", end - start)
 
 
-"""
-## Loading the model
-
-In this section we will load the [MPT-7B model](https://huggingface.co/mosaicml/mpt-7b).
-"""
-
-# load assets
-
-model_id = "mosaicml/mpt-7b"
-
-# mpt tokenizer load
-tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
-# set mpt tokenizer padding token to eos token
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.pad_token_id = tokenizer.eos_token_id
-print(f"{model_id} tokenizer model_max_length: ", tokenizer.model_max_length)
-
-# mpt llm load
-config = transformers.AutoConfig.from_pretrained(model_id, trust_remote_code=True)
-
-# custom options
-config.attn_config["attn_impl"] = "triton"  # Optional triton attention
-config.init_device = "cuda"  # For fast initialization directly on GPU!
-config.max_seq_len = tokenizer.model_max_length
-config.torch_dtype = "bfloat16"  # Set bfloat16 data type for sft
-
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    config=config,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-)
-
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max memory: ", max_memory)
-
-"""
-
-## Loading the trainer
-
-Here we will use the [`SFTTrainer` from TRL library](https://huggingface.co/docs/trl/main/en/sft_trainer) that gives a wrapper around transformers `Trainer` to easily fine-tune models on instruction based datasets. Let's first load the training arguments below.
-from transformers import TrainingArguments
-# see https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
-"""
-
-output_dir = "./results"
-num_train_epochs = 3
-auto_find_batch_size = True
-gradient_accumulation_steps = 1
-optim = "adamw_torch"
-save_strategy = "epoch"
-learning_rate = 5e-5
-lr_scheduler_type = "linear"
-warmup_ratio = 0.03
-logging_strategy = "steps"
-logging_steps = 50
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simple example of a single GPU training script."
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=5e-5,
+        help="Learning rate for training.",
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        default=3,
+        help="Num training epochs.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="Random seed.",
+    )
+    args = parser.parse_args()
+    print(f"Training args: {args}")
+    training_function(args)
 
 
-training_arguments = transformers.TrainingArguments(
-    output_dir=output_dir,
-    num_train_epochs=num_train_epochs,
-    auto_find_batch_size=auto_find_batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    optim=optim,
-    save_strategy=save_strategy,
-    learning_rate=learning_rate,
-    lr_scheduler_type=lr_scheduler_type,
-    warmup_ratio=warmup_ratio,
-    logging_strategy=logging_strategy,
-    logging_steps=logging_steps,
-)
-
-"""
-Then finally pass everything to the trainer
-"""
-
-max_seq_length = tokenizer.model_max_length
-
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=train_dataset,
-    formatting_func=formatting_prompts_func,
-    max_seq_length=max_seq_length,
-    tokenizer=tokenizer,
-    args=training_arguments,
-)
-
-"""
-## Train the model
-
-Now let's train the model! Simply call `trainer.train()`
-"""
-
-trainer.train()
-
-# finished: print GPU available memory and total time
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max memory: ", max_memory)
-
-end = time.time()
-print("total time: ", end - start)
+if __name__ == "__main__":
+    main()
--- sft/one_gpu/mpt-7b/sft-instruction-mpt-7b-openorca.py
+++ sft/one_gpu/mpt-7b/sft-instruction-mpt-7b-openorca.py
@@ -22,6 +22,7 @@ This script will only run on a workstation with 1 NVIDIA GPU and is not compatib
 
 import os
 import time
+import argparse
 
 start = time.time()
 
@@ -33,7 +34,7 @@ os.system("nvidia-smi")
 Run the cells below to setup and install the required libraries. For our experiment we will need `accelerate`, `transformers`, `datasets` and `trl` to leverage the recent [`SFTTrainer`](https://huggingface.co/docs/trl/main/en/sft_trainer). We will also install `einops` as it is a requirement to load MPT models, as well as `triton_pre_mlir` for triton optimized attention.
 """
 
-os.system("pip install -q -U trl transformers accelerate datasets einops")
+os.system("pip install -q -U trl transformers accelerate datasets einops pandas")
 os.system(
     "pip install -q -U triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mlir_sm90#subdirectory=python"
 )
@@ -41,186 +42,219 @@ os.system("pip list")
 
 # import libraries
 
-import pandas as pd
 import torch
 import transformers
 from datasets import load_dataset, Dataset
 from trl import SFTTrainer
-
-# print GPU available memory
-
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max_memory: ", max_memory)
-
-"""
-## Dataset
-
-For our experiment, we will use the `Open-Orca/OpenOrca` dataset to train general purpose instruct model.
-
-The dataset can be found [here](https://huggingface.co/datasets/Open-Orca/OpenOrca)
-"""
-
-dataset_name = "Open-Orca/OpenOrca"
-print(f"\nLoading {dataset_name} dataset...")
-dataset_orca = load_dataset(dataset_name, split="train", streaming=True)
-prompts = []
-responses = []
-dataset_orca
-
-# grab the firt 10k entries in an instruction format
-dataset_head = dataset_orca.take(200000)
-ids = []
-system_prompts = []
-questions = []
-responses = []
-
-for row in dataset_head:
-    ids.append(row["id"])
-    system_prompts.append(row["system_prompt"])
-    questions.append(row["question"])
-    responses.append(row["response"])
-
-pandas_dataset_orca = pd.DataFrame([ids, system_prompts, questions, responses]).T
-pandas_dataset_orca.columns = ["id", "system_prompt", "prompt", "response"]
-dataset_orca = Dataset.from_pandas(pandas_dataset_orca)
-
-# remove old text cols
-dataset_orca = dataset_orca.remove_columns(
-    [col for col in dataset_orca.column_names if col not in ["prompt", "response"]]
-)
-
-print("Print an example in the train dataset:")
-print(dataset_orca)
-print(dataset_orca[0])
-
-print("Final train dataset:")
-train_dataset = dataset_orca.shuffle(seed=42)
-print(train_dataset)
-print(train_dataset[0])
-print(train_dataset[-1])
-
-# let's now write a function to format the dataset for instruction fine-tuning
-# we will use the mpt-instruct model docs format
-# see https://huggingface.co/docs/trl/main/en/sft_trainer#format-your-input-prompts for docs
+import pandas as pd
 
 
-def formatting_prompts_func(dataset):
-    instructions = []
-    for i in range(len(dataset["prompt"])):
-        text = f"{dataset['prompt'][i]}\n{dataset['response'][i]}"
-        instructions.append(text)
-    return instructions
+def training_function(args):
+    lr = args.lr
+    num_epochs = args.num_epochs
+    seed = args.seed
+
+    transformers.set_seed(seed)
+
+    # print GPU available memory
+
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    """
+    ## Dataset
+    
+    For our experiment, we will use the `Open-Orca/OpenOrca` dataset to train general purpose instruct model.
+    
+    The dataset can be found [here](https://huggingface.co/datasets/Open-Orca/OpenOrca)
+    """
+
+    dataset_name = "Open-Orca/OpenOrca"
+    print(f"\nLoading {dataset_name} dataset...")
+    dataset_orca = load_dataset(dataset_name, split="train", streaming=True)
+
+    # grab the firt 200000 entries in an instruction format
+    dataset_head = dataset_orca.take(200000)
+    ids = []
+    system_prompts = []
+    questions = []
+    responses = []
+
+    for row in dataset_head:
+        ids.append(row["id"])
+        system_prompts.append(row["system_prompt"])
+        questions.append(row["question"])
+        responses.append(row["response"])
+
+    pandas_dataset_orca = pd.DataFrame([ids, system_prompts, questions, responses]).T
+    pandas_dataset_orca.columns = ["id", "system_prompt", "prompt", "response"]
+    dataset_orca = Dataset.from_pandas(pandas_dataset_orca)
+
+    # remove old text cols
+    dataset_orca = dataset_orca.remove_columns(
+        [col for col in dataset_orca.column_names if col not in ["prompt", "response"]]
+    )
+
+    print("Print an example in the train dataset:")
+    print(dataset_orca)
+    print(dataset_orca[0])
+
+    print("Final train dataset:")
+    train_dataset = dataset_orca.shuffle(seed=42)
+    print(train_dataset)
+    print(train_dataset[0])
+    print(train_dataset[-1])
+
+    # let's now write a function to format the dataset for instruction fine-tuning
+    # we will use the mpt-instruct model docs format
+    # see https://huggingface.co/docs/trl/main/en/sft_trainer#format-your-input-prompts for docs
+
+    def formatting_prompts_func(dataset):
+        instructions = []
+        for i in range(len(dataset["prompt"])):
+            text = f"{dataset['prompt'][i]}\n{dataset['response'][i]}"
+            instructions.append(text)
+        return instructions
+
+    """
+    ## Loading the model
+    
+    In this section we will load the [MPT-7B model](https://huggingface.co/mosaicml/mpt-7b).
+    """
+
+    # load assets
+
+    model_id = "mosaicml/mpt-7b"
+
+    # mpt tokenizer load
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+    # set mpt tokenizer padding token to eos token
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    print(f"{model_id} tokenizer model_max_length: ", tokenizer.model_max_length)
+
+    # mpt llm load
+    config = transformers.AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+
+    # custom options
+    config.attn_config["attn_impl"] = "triton"  # Optional triton attention
+    config.init_device = "meta"  # For fast initialization
+    config.max_seq_len = tokenizer.model_max_length
+    config.torch_dtype = "bfloat16"  # Set bfloat16 data type for sft
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        config=config,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    """
+    
+    ## Loading the trainer
+    
+    Here we will use the [`SFTTrainer` from TRL library](https://huggingface.co/docs/trl/main/en/sft_trainer) that gives a wrapper around transformers `Trainer` to easily fine-tune models on instruction based datasets. Let's first load the training arguments below.
+    from transformers import TrainingArguments
+    # see https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
+    """
+
+    output_dir = "./results"
+    num_train_epochs = num_epochs
+    auto_find_batch_size = True
+    gradient_accumulation_steps = 1
+    optim = "adamw_torch"
+    save_strategy = "epoch"
+    learning_rate = lr
+    lr_scheduler_type = "linear"
+    warmup_ratio = 0.03
+    logging_strategy = "steps"
+    logging_steps = 50
+
+    training_arguments = transformers.TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=num_train_epochs,
+        auto_find_batch_size=auto_find_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim=optim,
+        save_strategy=save_strategy,
+        learning_rate=learning_rate,
+        lr_scheduler_type=lr_scheduler_type,
+        warmup_ratio=warmup_ratio,
+        logging_strategy=logging_strategy,
+        logging_steps=logging_steps,
+    )
+
+    """
+    Then finally pass everything to the trainer
+    """
+
+    max_seq_length = tokenizer.model_max_length
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset,
+        formatting_func=formatting_prompts_func,
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+    )
+
+    """
+    ## Train the model
+    
+    Now let's train the model! Simply call `trainer.train()`
+    """
+
+    trainer.train()
+
+    # finished: print GPU available memory and total time
+    free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
+    max_memory = f"{free_in_GB-2}GB"
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: max_memory for i in range(n_gpus)}
+    print("Max VRAM (GB): ", max_memory)
+
+    end = time.time()
+    print("Total time (sec): ", end - start)
 
 
-"""
-## Loading the model
-
-In this section we will load the [MPT-7B model](https://huggingface.co/mosaicml/mpt-7b).
-"""
-
-# load assets
-
-model_id = "mosaicml/mpt-7b"
-
-# mpt tokenizer load
-tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
-# set mpt tokenizer padding token to eos token
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.pad_token_id = tokenizer.eos_token_id
-print(f"{model_id} tokenizer model_max_length: ", tokenizer.model_max_length)
-
-# mpt llm load
-config = transformers.AutoConfig.from_pretrained(model_id, trust_remote_code=True)
-
-# custom options
-config.attn_config["attn_impl"] = "triton"  # Optional triton attention
-config.init_device = "cuda"  # For fast initialization directly on GPU!
-config.max_seq_len = tokenizer.model_max_length
-config.torch_dtype = "bfloat16"
-
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    config=config,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-)
-
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max memory: ", max_memory)
-
-"""
-
-## Loading the trainer
-
-Here we will use the [`SFTTrainer` from TRL library](https://huggingface.co/docs/trl/main/en/sft_trainer) that gives a wrapper around transformers `Trainer` to easily fine-tune models on instruction based datasets. Let's first load the training arguments below.
-from transformers import TrainingArguments
-# see https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
-"""
-
-output_dir = "./results"
-num_train_epochs = 1
-auto_find_batch_size = True
-gradient_accumulation_steps = 1
-optim = "adamw_torch"
-save_strategy = "epoch"
-learning_rate = 5e-5
-lr_scheduler_type = "linear"
-warmup_ratio = 0.03
-logging_strategy = "steps"
-logging_steps = 50
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simple example of a single GPU training script."
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=5e-5,
+        help="Learning rate for training.",
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        default=1,
+        help="Num training epochs.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="Random seed.",
+    )
+    args = parser.parse_args()
+    print(f"Training args: {args}")
+    training_function(args)
 
 
-training_arguments = transformers.TrainingArguments(
-    output_dir=output_dir,
-    num_train_epochs=num_train_epochs,
-    auto_find_batch_size=auto_find_batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    optim=optim,
-    save_strategy=save_strategy,
-    learning_rate=learning_rate,
-    lr_scheduler_type=lr_scheduler_type,
-    warmup_ratio=warmup_ratio,
-    logging_strategy=logging_strategy,
-    logging_steps=logging_steps,
-)
-
-"""
-Then finally pass everything to the trainer
-"""
-
-max_seq_length = tokenizer.model_max_length
-
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=train_dataset,
-    formatting_func=formatting_prompts_func,
-    max_seq_length=max_seq_length,
-    tokenizer=tokenizer,
-    args=training_arguments,
-)
-
-"""
-## Train the model
-
-Now let's train the model! Simply call `trainer.train()`
-"""
-
-trainer.train()
-
-# finished: print GPU available memory and total time
-free_in_GB = int(torch.cuda.mem_get_info()[0] / 1024**3)
-max_memory = f"{free_in_GB-2}GB"
-n_gpus = torch.cuda.device_count()
-max_memory = {i: max_memory for i in range(n_gpus)}
-print("max memory: ", max_memory)
-
-end = time.time()
-print("total time: ", end - start)
+if __name__ == "__main__":
+    main()
