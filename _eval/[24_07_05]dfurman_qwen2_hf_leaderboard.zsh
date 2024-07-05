b8b101a4219f8a69f5441a68facacafcbfc10808
daniel-furman <dryanfurman@gmail.com>
2024-07-05 15:29:43 -0700

    latest code

---
+++ _eval/[24_07_05]dfurman_qwen2_hf_leaderboard.zsh
@@ -0,0 +1,9 @@
+lm_eval --model hf \
+    --model_args pretrained=dfurman/Qwen2-72B-Orpo-v0.1,parallelize=True,attn_implementation="flash_attention_2",dtype="bfloat16" \
+    --apply_chat_template \
+    --system_instruction "You are a helpful assistant." \
+    --tasks leaderboard \
+    --batch_size 2 \
+    --log_samples \
+    --output_path results \
+    --hf_hub_log_args hub_results_org=dfurman,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=True \
---
+++ _eval/setup.zsh
@@ -0,0 +1,4 @@
+git clone https://github.com/EleutherAI/lm-evaluation-harness
+cd lm-evaluation-harness
+pip install -e .
+pip install -qqq flash-attn
